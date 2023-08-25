069a134471b16d4405d9b2d07f1e32c02c31ae8b
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2023-08-25 13:04:57 +0200

    mobile

--- dev/www/src/components/Waitlist.tsx
+++ dev/www/src/components/Waitlist.tsx
@@ -1,7 +1,7 @@
 export const WaitList = () => {
   return (
     <form
-      className="flex gap-6 mt-14 mb-7"
+      className="gap-6 mt-14 mb-7 xl:flex space-y-4 xl:space-y-0"
       onSubmit={(ev) => {
         ev.preventDefault();
 
@@ -10,11 +10,11 @@ export const WaitList = () => {
     >
       <input
         type="text"
-        className="border rounded-md bg-transparent p-3 w-1/3"
+        className="border rounded-md bg-transparent p-3 xl:w-1/3 w-full"
         placeholder="Waitlist (Email)"
       />
       <button
-        className="py-3 px-8 bg-gradient-to-tr from-[#defff8] to-[#b1fee9] text-gray-900 gap-4 rounded-md"
+        className="py-3 px-8 bg-gradient-to-tr from-[#defff8] to-[#b1fee9] text-gray-900 gap-4 rounded-md xl:w-auto w-full"
         type="submit"
       >
         <svg
@@ -23,6 +23,7 @@ export const WaitList = () => {
           height="24"
           fill="#0a0606"
           viewBox="0 0 256 256"
+          className="mx-auto"
         >
           <path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32Zm0,16V152h-28.7A15.86,15.86,0,0,0,168,156.69L148.69,176H107.31L88,156.69A15.86,15.86,0,0,0,76.69,152H48V48Zm0,160H48V168H76.69L96,187.31A15.86,15.86,0,0,0,107.31,192h41.38A15.86,15.86,0,0,0,160,187.31L179.31,168H208v40Z"></path>
         </svg>
